d4ed574b4f0bba9f9ca0b9df45c5f26a8f111933
daniel.mcweeney <daniel.mcweeney@a89e8a5f-081a-0410-95c2-51f272165fb4>
2007-02-01 00:38:48 +0000

    Initial Testing of SAPLink badge

---
+++ saplinkBadge.js
@@ -0,0 +1,41 @@
+	function loadXMLDoc(url) {
+		req = false;
+	    // branch for native XMLHttpRequest object
+	    if(window.XMLHttpRequest && !(window.ActiveXObject)) {
+	    	try {
+				req = new XMLHttpRequest();
+	        } catch(e) {
+				req = false;
+	        }
+	    // branch for IE/Windows ActiveX version
+	    } else if(window.ActiveXObject) {
+	       	try {
+	        	req = new ActiveXObject("Msxml2.XMLHTTP");
+	      	} catch(e) {
+	        	try {
+	          		req = new ActiveXObject("Microsoft.XMLHTTP");
+	        	} catch(e) {
+	          		req = false;
+	        	}
+			}
+	    }
+		if(req) {
+			req.onreadystatechange = processReqChange;
+			req.open("GET", url, true);
+			req.send("");
+		}
+	}
+	
+	function processReqChange() {
+    // only if req shows "loaded"
+    if (req.readyState == 4) {
+        // only if "OK"
+        if (req.status == 200) {
+            alert ('got a doc');
+        } else {
+            alert("There was a problem retrieving the XML data:\n" +
+                req.statusText);
+        }
+    }
+}
+loadXMLDoc('http://code.google.com/p/saplink/downloads/list');	
